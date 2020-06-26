a5c5dbc7540aa0a7c207b48fedafd00f05107e54
Quarmuz <55035059+Quarmuz@users.noreply.github.com>
2020-06-26 21:00:07 +0400

    Update code.ino

--- June21_Communication/code.ino
+++ June21_Communication/code.ino
@@ -1,3 +1,13 @@
+//================================================================//
+//
+// The code translates the motion of a servo motor to string and
+// sends it as a string to be read by Processing. Since negative
+// voltage is not noted, the left and right motion have sepparate
+// inputs, where left-turn sign is being flipped.
+//
+//================================================================//
+
+
 void setup() {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
